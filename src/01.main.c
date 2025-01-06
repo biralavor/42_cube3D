56ce57715cc8c6619c0765ceb1ab4e149f527778
biralavor <biralavor@gmail.com>
2025-01-06 13:57:01 -0300

    adding arguments into main()

--- src/01.main.c
+++ src/01.main.c
@@ -1,8 +1,28 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   01.main.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umeneses <umenses@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 13:13:28 by umeneses          #+#    #+#             */
+/*   Updated: 2025/01/06 13:13:28 by umeneses         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube3d.h"
 
 
-int main(void)
+int main(int ac, char **av)
 {
+	if (ac != 2)
+	{
+		printf("Cube3D needs a map file to starts the game session.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("I grab the file: %s\n", av[1]);
+	
 	printf("Hello, Cube3D!\n");
 	return (0);
 }
