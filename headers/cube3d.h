35421aa5ce7fff7ba4cd1e5f9e0359755a642c5d
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2025-01-06 19:02:10 -0300

    ajusted header and add new maps invalid

--- headers/cube3d.h
+++ headers/cube3d.h
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   cube3d.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/06 13:13:33 by umeneses          #+#    #+#             */
-/*   Updated: 2025/01/06 18:41:55 by umeneses         ###   ########.fr       */
+/*   Created: 2025/01/06 18:52:38 by gigardin          #+#    #+#             */
+/*   Updated: 2025/01/06 18:52:50 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 
 #ifndef CUBE3D_H
 # define CUBE3D_H
 
 # include "../libs/libft/includes/libft.h"
+# include "../libs/codam/include/MLX42/MLX42.h"
 # include <stdio.h>
 # include <fcntl.h>
 # include <stdlib.h>
@@ -29,5 +29,16 @@ typedef struct s_map
 
 bool	map_validation(int ac, char **av, t_map *map);
 bool	existing_mapfile(int ac, char **argv, t_map *map);
+
+
+
+// struct for all datas
+typedef struct s_data
+{
+	mlx_t			*mlx;
+	int				config_stage;
+	t_map			*map;
+	t_map			*map_clone;
+}	t_data;
 
 #endif
