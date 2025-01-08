a6d0cb3b791bb76b56c0f20f9ac2342380997e4a
biralavor <umeneses@student.42sp.org.br>
2025-01-08 18:28:31 -0300

    adding new verification - valid extension checker

--- src/map_validation/map_validation.c
+++ src/map_validation/map_validation.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umenses@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 18:36:07 by umeneses          #+#    #+#             */
-/*   Updated: 2025/01/08 17:58:27 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/01/08 18:28:41 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@ bool	map_validation(int ac, char **av, t_map *map)
 {
 	if (map_opener(ac, av, map)
 		&& map_reader(map)
-		&& unique_def_compass(map))
+		&& unique_def_compass(map)
+		&& valid_extension_checker(map))
 	{
-		//check_extension_valid(map);
-		//check_file_content_map(map);
-		// add map functions here
+		// check_file_content_map(map);
 		map_printer(map);
 		return (true);
 	}
