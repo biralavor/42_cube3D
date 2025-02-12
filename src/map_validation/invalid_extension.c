2e39becf04df7bd2c30d824694a53f9b021fceb4
biralavor <biralavor@gmail.com>
2025-02-12 19:34:01 -0300

    clear comments

--- src/map_validation/invalid_extension.c
+++ src/map_validation/invalid_extension.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umenses@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 18:32:29 by gigardin          #+#    #+#             */
-/*   Updated: 2025/01/08 19:03:00 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/02/12 19:33:36 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,3 @@ bool	valid_extension_checker(char **av)
 	ft_putendl_fd(RED"Map not in '.cub' extension!"RESET, STDOUT_FILENO);
 	return (false);
 }
-
-// bool	check_file_content_map(t_map *map)
-// {
-// 	int		fd;
-// 	char	buffer[1];
-// 	// int		i;
-
-// 	// i = ft_strlen(map->file);
-// 	fd = open(map->buffer, O_RDONLY);
-// 	if (read(fd, buffer, 1) == 0)
-// 	{
-// 		close(fd);
-// 		ft_putendl_fd("Map file is empty!", STDOUT_FILENO);
-// 		return (false);
-// 	}
-// 	close(fd);
-// 	return (true);
-// }
