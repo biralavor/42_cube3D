898c8f9798172a066a99fe7ddc0f26e1d9a14433
biralavor <biralavor@gmail.com>
2025-02-12 19:35:35 -0300

    Update variable name

--- src/map_validation/invalid_extension.c
+++ src/map_validation/invalid_extension.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umenses@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 18:32:29 by gigardin          #+#    #+#             */
-/*   Updated: 2025/02/12 19:33:36 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/02/12 19:34:18 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 bool	valid_extension_checker(char **av)
 {
-	int		i;
+	int		idx;
 	char	*map_name;
 
-	i = ft_strlen(av[1]);
+	idx = ft_strlen(av[1]);
 	map_name = av[1];
-	if (ft_strncmp(&map_name[i - 4], ".cub", 4) == 0)
+	if (ft_strncmp(&map_name[idx - 4], ".cub", 4) == 0)
 		return (true);
 	ft_putendl_fd(RED"Map not in '.cub' extension!"RESET, STDOUT_FILENO);
 	return (false);
