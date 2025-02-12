68a7b5634898fafd61b584634edff0ecda5ae3bc
biralavor <biralavor@gmail.com>
2025-02-12 14:27:20 -0300

    #71 fix invalid read

--- src/02.mem_struct_init.c
+++ src/02.mem_struct_init.c
@@ -16,7 +16,7 @@ void	memory_struct_init(t_map *map)
 {
 	int	idx;
 
-	map->gamemap = (char **)ft_calloc(MAX_MAP_HEIGHT, sizeof(char *));
+	map->gamemap = (char **)ft_calloc(MAX_MAP_HEIGHT + 1, sizeof(char *));
 	map->ggraph = (char **)ft_calloc(MAX_TEXTURE_PATH, sizeof(char *));
 	map->colors = (char **)ft_calloc(MAX_COLOR_DIGIT, sizeof(char *));
 	if (!map->gamemap || !map->ggraph  || !map->colors)
@@ -24,21 +24,21 @@ void	memory_struct_init(t_map *map)
 	idx = -1;
 	while (++idx < MAX_MAP_HEIGHT)
 	{
-		map->gamemap[idx] = (char *)ft_calloc(MAX_MAP_WIDTH, sizeof(char *));
+		map->gamemap[idx] = (char *)ft_calloc(MAX_MAP_WIDTH, sizeof(char));
 		if (!map->gamemap[idx])
 			ft_error_msg("Malloc failed at gamemap columns\n");
 	}
 	idx = -1;
 	while (++idx < 4)
 	{
-		map->ggraph[idx] = (char *)ft_calloc(MAX_TEXTURE_PATH, sizeof(char *));
+		map->ggraph[idx] = (char *)ft_calloc(MAX_TEXTURE_PATH, sizeof(char));
 		if (!map->ggraph[idx])
 			ft_error_msg("Malloc failed at gamegraph columns\n");
 	}
 	idx = -1;
 	while (++idx < 4)
 	{
-		map->colors[idx] = (char *)ft_calloc(MAX_COLOR_DIGIT, sizeof(char *));
+		map->colors[idx] = (char *)ft_calloc(MAX_COLOR_DIGIT, sizeof(char));
 		if (!map->colors[idx])
 			ft_error_msg("Malloc failed at colors columns\n");
 	}
