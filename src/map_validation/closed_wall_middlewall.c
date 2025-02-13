39b3f4c955d37488bb7c7b41b8be63a15ea287c4
biralavor <biralavor@gmail.com>
2025-02-13 17:14:53 -0300

    #104 fix too many functions in the file

--- src/map_validation/closed_wall_middlewall.c
+++ src/map_validation/closed_wall_middlewall.c
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   closed_wall_manager.c                              :+:      :+:    :+:   */
+/*   closed_wall_middlewall.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umenses@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 17:21:47 by umeneses          #+#    #+#             */
-/*   Updated: 2025/02/13 17:07:32 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/02/13 17:12:04 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
-
-bool	top_wall_finder(char **arr, char w)
-{
-	int		x;
-
-	x = 0;
-	if (arr[0][x] == w)
-	{
-		while (arr[0][++x] && arr[0][x] == w)
-		{
-			if (!arr[0][x + 1])
-				return (true);
-		}
-	}
-	ft_putstr_fd(RED"Your MAP has a breach on the top wall", STDOUT_FILENO);
-	return (false);
-}
 
 bool	middle_wall_finder(char **arr, char w)
 {
@@ -97,33 +80,5 @@ bool	middle_max_boundary(char **arr, int y, int x, char w)
 	if (!arr[y + 1][x] && !arr[y - 1][x]
 		&& arr[y - 1][x - 1] == w && arr[y + 1][x - 1] == w)
 		return (true);
-	return (false);
-}
-
-bool	bottom_wall_finder(char **arr, char w)
-{
-	int		y;
-	int		x;
-
-	y = 0;
-	x = 0;
-	while (arr[y][x] && arr[y + 1][x] && arr[y + 1][x] != '\n')
-		y++;
-	if (arr[y][x] == w)
-	{
-		while (arr[y][x] && arr[y][x] == w)
-		{
-			if ((!arr[y][x + 1] || arr[y][x + 1] == '\n')
-				&& (!arr[y + 1][0] || arr[y + 1][0] == '\n'))
-			{
-				if (arr[y][x + 1] == '\n' && linebreak_reader(arr, y, x))
-					return (true);
-				else if (!arr[y][x + 1])
-					return (true);
-			}
-			x++;
-		}
-	}
-	ft_putstr_fd(RED"Your MAP has a breach on the bottom wall", STDOUT_FILENO);
 	return (false);
 }
