6611bc8d88723f6b93c871c6ce2c159a87f1d160
biralavor <biralavor@gmail.com>
2025-02-17 13:22:19 -0300

    remove magic number

--- src/02.struct_mem_alloc.c
+++ src/02.struct_mem_alloc.c
@@ -12,7 +12,7 @@
 
 #include "cube3d.h"
 
-static void	inside_pointers_alloc(char **array, int idx, int size,
+static void	inside_ptrs_alloc(char **array, int idx, int size,
 				int alloc_nitems);
 
 void	struct_memory_alloc(t_map *map)
@@ -25,17 +25,17 @@ void	struct_memory_alloc(t_map *map)
 	if (!map->gamemap || !map->ggraph || !map->colors)
 		ft_error_msg("Malloc failed at memory struct init\n");
 	idx = -1;
-	inside_pointers_alloc(map->gamemap, idx, MAX_MAP_HEIGHT, MAX_MAP_WIDTH);
+	inside_ptrs_alloc(map->gamemap, idx, MAX_MAP_HEIGHT, MAX_MAP_WIDTH);
 	idx = -1;
-	inside_pointers_alloc(map->ggraph, idx, 4, MAX_TEXTURE_PATH);
+	inside_ptrs_alloc(map->ggraph, idx, MAX_INSIDE_ALLOC, MAX_TEXTURE_PATH);
 	idx = -1;
-	inside_pointers_alloc(map->colors, idx, 4, MAX_COLOR_DIGIT);
+	inside_ptrs_alloc(map->colors, idx, MAX_INSIDE_ALLOC, MAX_COLOR_DIGIT);
 }
 
-static void	inside_pointers_alloc(char **array, int idx, int size,
+static void	inside_ptrs_alloc(char **array, int idx, int max_alloc,
 				int alloc_nitems)
 {
-	while (++idx < size)
+	while (++idx < max_alloc)
 	{
 		array[idx] = (char *)ft_calloc(alloc_nitems, sizeof(char));
 		if (!array[idx])
