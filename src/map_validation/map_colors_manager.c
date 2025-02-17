e350959057bcab86c036071aeddc0b5df53d17a6
biralavor <biralavor@gmail.com>
2025-02-17 19:37:24 -0300

    rename file

--- src/map_validation/map_colors_manager.c
+++ src/map_validation/map_colors_manager.c
@@ -1,24 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   map_validation_part2.c                             :+:      :+:    :+:   */
+/*   map_colors_manager.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umenses@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 18:36:07 by umeneses          #+#    #+#             */
-/*   Updated: 2025/02/13 17:24:16 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/02/17 19:35:29 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
-
-bool	no_garbage_manager(t_map *map)
-{
-	if (no_garbage_at_texture(map->ggraph)
-		&& no_garbage_at_gamemap(map->gamemap))
-		return (true);
-	return (false);
-}
 
 bool	map_colors_manager(char **arr)
 {
