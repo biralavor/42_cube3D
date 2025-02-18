f0cc3b4e4fb8ae67bdb81002ff761a339e23c742
biralavor <biralavor@gmail.com>
2025-02-17 22:33:59 -0300

    refactor function file sequence

--- src/map_validation/map_colors.c
+++ src/map_validation/map_colors.c
@@ -6,32 +6,11 @@
 /*   By: umeneses <umenses@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 18:49:09 by umeneses          #+#    #+#             */
-/*   Updated: 2025/02/13 14:49:19 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/02/17 22:10:51 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
-
-bool	color_digits_quantity_checker(int *color_digits)
-{
-	int			id;
-	static int	quant_approved[6];
-
-	id = -1;
-	while (++id < 6)
-	{
-		if (color_digits[id] >= 1 && color_digits[id] <= 3)
-		{
-			quant_approved[id]++;
-			if (quant_approved[0] == 1 && quant_approved[1] == 1
-				&& quant_approved[2] == 1 && quant_approved[3] == 1
-				&& quant_approved[4] == 1 && quant_approved[5] == 1)
-				return (true);
-		}
-	}
-	ft_putstr_fd(YEL"Invalid number of digits at Color Map", STDERR_FILENO);
-	return (false);
-}
 
 void	color_digits_counter(char **arr, int *color_digits)
 {
@@ -57,6 +36,27 @@ void	color_digits_counter(char **arr, int *color_digits)
 				break ;
 		}
 	}
+}
+
+bool	color_digits_quantity_checker(int *color_digits)
+{
+	int			id;
+	static int	quant_approved[6];
+
+	id = -1;
+	while (++id < 6)
+	{
+		if (color_digits[id] >= 1 && color_digits[id] <= 3)
+		{
+			quant_approved[id]++;
+			if (quant_approved[0] == 1 && quant_approved[1] == 1
+				&& quant_approved[2] == 1 && quant_approved[3] == 1
+				&& quant_approved[4] == 1 && quant_approved[5] == 1)
+				return (true);
+		}
+	}
+	ft_putstr_fd(YEL"Invalid number of digits at Color Map", STDERR_FILENO);
+	return (false);
 }
 
 bool	color_values_into_array(char **arr, int *color_digits, int *rgbrgb)
