0bd85a03923fa6807258a0e0cade1ec7d9a42387
Bira Lavor <umeneses@student.42sp.org.br>
2025-04-21 20:39:51 -0300

    testing space fix

--- src/graphic/minimap.c
+++ src/graphic/minimap.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 20:16:07 by gigardin          #+#    #+#             */
-/*   Updated: 2025/04/21 20:33:30 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/04/21 20:39:22 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -62,8 +62,8 @@ void	minimap_player_render(t_game *game)
 		while (dx <= 2)
 		{
 			if ((px + dx) >= 0 && (py + dy) >= 0)
-				mlx_put_pixel(game->mlx_image,\
-					(px + dx), (py + dy), PLAYER_POSITION_COLOR);
+				mlx_put_pixel(game->mlx_image, \
+				(px + dx), (py + dy), PLAYER_POSITION_COLOR);
 			dx++;
 		}
 		dy++;
@@ -102,9 +102,10 @@ static void	player_line_direction_designer(t_game *game, int px, int py)
 			draw_x = px + box_x;
 			draw_y = py + box_y;
 			if (draw_x >= 0 && draw_y >= 0 \
-				&& draw_x < MAX_MAP_WIDTH && draw_y < MAX_MAP_HEIGHT)
+			 && draw_x < MAX_MAP_WIDTH && draw_y < MAX_MAP_HEIGHT)
 			{
-				mlx_put_pixel(game->mlx_image, draw_x, draw_y, PLAYER_DIRECTION_COLOR);
+				mlx_put_pixel(game->mlx_image, draw_x, draw_y, \
+				 PLAYER_DIRECTION_COLOR);
 			}
 			box_x++;
 		}
