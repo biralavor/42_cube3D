f2a3a9595c9badf6b312edfb5d1772d7a4bbd5d9
Bira Lavor <umeneses@student.42sp.org.br>
2025-04-21 22:30:04 -0300

    add a memory leak to test actions

--- src/graphic/wall.c
+++ src/graphic/wall.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wall.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
+/*   By: umeneses <umeneses@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 19:06:11 by gigardin          #+#    #+#             */
-/*   Updated: 2025/04/21 18:01:05 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/04/21 22:29:24 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -49,7 +49,7 @@ static char	**populate_texture_path(t_map *map)
 	{
 		temp = ft_split(map->ggraph[idx], ' ');
 		textures[id] = ft_strdup(temp[1]);
-		ft_free_array(temp);
+		// ft_free_array(temp);
 		temp = NULL;
 		id++;
 	}
