ffadad22cc5875b0d82a9a9368cf6b1e52f47b3a
Bira Lavor <umeneses@student.42sp.org.br>
2025-04-23 14:55:45 -0300

    refactor variable name

--- src/graphic/hooks_utils.c
+++ src/graphic/hooks_utils.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 00:43:16 by gigardin          #+#    #+#             */
-/*   Updated: 2025/04/23 14:51:13 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/04/23 14:55:21 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ void	handle_mouse_direction(double m_xpos, double m_ypos, void *param)
 {
 	t_game			*game;
 	static double	last_mouse_xpos = -1;
-	double			sensibility;
+	double			sensitivity;
 	double			rotation;
 
 	game = (t_game *)param;
-	sensibility = 0.015;
+	sensitivity = 0.015;
 	(void)m_ypos;
 	if (last_mouse_xpos < 0)
 		last_mouse_xpos = m_xpos;
-	rotation = (m_xpos - last_mouse_xpos) * sensibility;
+	rotation = (m_xpos - last_mouse_xpos) * sensitivity;
 	if (fabs(rotation) > 0.0001)
 	{
 		rotate_player(game, rotation);
